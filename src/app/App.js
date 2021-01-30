f60cbee06816feda9a5644ca535a4e72f8738ba9
Alex <am2invest@yandex.ru>
2021-01-30 13:45:39 +0500

    добавил разметку на день

--- src/app/App.js
+++ src/app/App.js
@@ -7,7 +7,22 @@ function App(){
 <>
   <Header />
   <main>
-    Контент
+    <div className="pay-list">
+      <div className="pay-item">
+          <div className="pay-item__date">
+            <div className="pay-item__day">07</div>
+            <div className="pay-item__month">ЯНВ</div>
+            <div className="pay-item__year">20</div>
+          </div>
+          <div className="pay-item__info">
+            <div className="pay-item__positive">+ 3000</div>
+            <div className="pay-item__negative">- 543</div>
+          </div>
+          <div className="saldo">
+            +2457
+          </div>
+      </div>
+    </div>
   </main>
 </>
   )
