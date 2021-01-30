b86d15f34c1db984ec92b932e5e1da9c7f7ec882
Alex <am2invest@yandex.ru>
2021-01-30 16:07:12 +0500

    добавление класса в зависимости от условия и вывод props в html

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 import Header from '../header/Header';
+import PayItem from '../pay-item/PayItem';
+
+
+const payItems = [
+  {date: '01', month: 'ЯНВ', year: '20', positive: '3000', negative: '543'},
+  {date: '22', month: 'ФЕВ', year: '20', positive: '9553', negative: '17350'},
+  {date: '13', month: 'МАР', year: '20', positive: '7500', negative: '9634'},
+  {date: '07', month: 'СЕН', year: '20', positive: '14500', negative: '7532'},
+  {date: '29', month: 'ОКТ', year: '20', positive: '700', negative: '12487'},
+  {date: '17', month: 'ДЕК', year: '20', positive: '23000', negative: '4567'}
+];
 
 
 function App(){
@@ -8,20 +19,7 @@ function App(){
   <Header />
   <main>
     <div className="pay-list">
-      <div className="pay-item">
-          <div className="pay-item__date">
-            <div className="pay-item__day">07</div>
-            <div className="pay-item__month">ЯНВ</div>
-            <div className="pay-item__year">20</div>
-          </div>
-          <div className="pay-item__info">
-            <div className="pay-item__positive">+ 3000</div>
-            <div className="pay-item__negative">- 543</div>
-          </div>
-          <div className="pay-item__saldo">
-            +2457
-          </div>
-      </div>
+      <PayItem data={payItems[1]}/>
     </div>
   </main>
 </>
