ede19114548095334bbce11c37c9fcc4acf6e163
Alex <am2invest@yandex.ru>
2021-01-30 16:20:10 +0500

    декомпозировали props, вывели весь список

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,12 @@ import PayItem from '../pay-item/PayItem';
 
 
 const payItems = [
-  {date: '01', month: 'ЯНВ', year: '20', positive: '3000', negative: '543'},
-  {date: '22', month: 'ФЕВ', year: '20', positive: '9553', negative: '17350'},
-  {date: '13', month: 'МАР', year: '20', positive: '7500', negative: '9634'},
-  {date: '07', month: 'СЕН', year: '20', positive: '14500', negative: '7532'},
-  {date: '29', month: 'ОКТ', year: '20', positive: '700', negative: '12487'},
-  {date: '17', month: 'ДЕК', year: '20', positive: '23000', negative: '4567'}
+  {day: '01', month: 'ЯНВ', year: '20', positive: '3000', negative: '543'},
+  {day: '22', month: 'ФЕВ', year: '20', positive: '9553', negative: '17350'},
+  {day: '13', month: 'МАР', year: '20', positive: '7500', negative: '9634'},
+  {day: '07', month: 'СЕН', year: '20', positive: '14500', negative: '7532'},
+  {day: '29', month: 'ОКТ', year: '20', positive: '700', negative: '12487'},
+  {day: '17', month: 'ДЕК', year: '20', positive: '23000', negative: '4567'}
 ];
 
 
@@ -19,7 +19,7 @@ function App(){
   <Header />
   <main>
     <div className="pay-list">
-      <PayItem data={payItems[1]}/>
+      {payItems.map(payItem => <PayItem day={payItem.day} month={payItem.month} year={payItem.year} positive={payItem.positive} negative={payItem.negative}/>)}
     </div>
   </main>
 </>
